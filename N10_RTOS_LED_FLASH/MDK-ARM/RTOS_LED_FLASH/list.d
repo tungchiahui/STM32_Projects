5d32d8bcee38487dba1a0b829a0ceb31cea44878
tungchiahui <tungchiahui@gmail.com>
2024-02-01 17:46:02 +0800

    Merge branch 'main' of https://github.com/tungchiahui/ST-Cube-MX

---
+++ N10_RTOS_LED_FLASH/MDK-ARM/RTOS_LED_FLASH/list.d
@@ -0,0 +1,12 @@
+rtos_led_flash/list.o: ..\Middlewares\Third_Party\FreeRTOS\Source\list.c \
+  D:\Program\ Files\Arm\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\FreeRTOS.h \
+  D:\Program\ Files\Arm\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  D:\Program\ Files\Arm\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\Core\Inc\FreeRTOSConfig.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\projdefs.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\portable.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\deprecated_definitions.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\portable\RVDS\ARM_CM4F\portmacro.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\mpu_wrappers.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\list.h
---
+++ N10_RTOS_LED_FLASH/MDK-ARM/RTOS_LED_FLASH/tasks.d
@@ -0,0 +1,17 @@
+rtos_led_flash/tasks.o: \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\tasks.c \
+  D:\Program\ Files\Arm\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  D:\Program\ Files\Arm\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\FreeRTOS.h \
+  D:\Program\ Files\Arm\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  D:\Program\ Files\Arm\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\Core\Inc\FreeRTOSConfig.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\projdefs.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\portable.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\deprecated_definitions.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\portable\RVDS\ARM_CM4F\portmacro.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\mpu_wrappers.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\task.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\list.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\timers.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\stack_macros.h
